54df9b0268463e2f7eb95117c8a1fad758497f60
mohamad ali <mohamadalitellawi@gmail.com>
2021-04-17 17:01:46 +0300

    add ef core and dbcontext

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext: DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
