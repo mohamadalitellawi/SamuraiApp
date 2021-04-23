5017515bccd40e56f85aaa575f791afe07a0aebd
mohamad ali <mohamadalitellawi@gmail.com>
2021-04-23 16:19:40 +0300

    add migration

--- SamuraiApp.Data/Migrations/20210423131846_many_to_many_simple.Designer.cs
+++ SamuraiApp.Data/Migrations/20210423131846_many_to_many_simple.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SamuraiApp.Data;
 
 namespace SamuraiApp.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20210423131846_many_to_many_simple")]
+    partial class many_to_many_simple
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
